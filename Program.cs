d6b61a7e227a4f0750959cd27fd52fec964efd0f
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-01-21 09:53:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            b = 10;
+            c = 3;
+            c = c * b;
+            a = 10 * c % 10;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
